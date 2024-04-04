a7430e951c43ed6b16ebe8c1faebcb5a58d1883a
braanj <anjar.bra@gmail.com>
2024-04-04 22:07:00 +0200

    Preload featured image

--- components/content/HeadlineImage.vue
+++ components/content/HeadlineImage.vue
@@ -16,12 +16,26 @@ const { data: featuredImage } = useAsyncData("featuredImage", (_) => {
     },
   });
 
-  return { backgroundImage: `url('${imgUrl}')` };
+  return imgUrl;
+});
+
+const bgStyles = computed(() => ({
+  backgroundImage: `url('${featuredImage.value}')`,
+}));
+
+useHead({
+  link: [
+    {
+      rel: "preload",
+      as: "image",
+      href: featuredImage.value,
+    },
+  ],
 });
 </script>
 
 <template>
-  <div class="pt-80 bg-cover bg-center" :style="featuredImage">
+  <div class="pt-80 bg-cover bg-center" :style="bgStyles">
     <div class="bottom-0 p-6 max-w-[1200px] mx-auto">
       <h1 class="text-white font-bold text-4xl">{{ content.externalName }}</h1>
       <p class="text-white text-xl">{{ content.subtitle }}</p>
