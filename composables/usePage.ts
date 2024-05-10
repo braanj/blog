e22917b0809651e2293521f2a4ee16939bd0520d
braanj <anjar.bra@gmail.com>
2024-05-11 00:16:00 +0200

    Remove clutter

--- composables/usePage.ts
+++ composables/usePage.ts
@@ -1,6 +1,4 @@
 export default async <T>(slug: string) => {
-  console.log(slug);
-
   const { data } = useAsyncData(`${slug}-page-data`, async () => {
     const contentContainers = await useFetchWithCache(`/api/discover/${slug}`);
     return contentContainers;
