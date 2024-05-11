ed671d6cdb7f5f438bca9dd69eb109f62991e691
braanj <anjar.bra@gmail.com>
2024-05-11 19:02:47 +0200

    Refactor data fetching

--- composables/useHomepage.ts
+++ composables/useHomepage.ts
@@ -1,8 +1,4 @@
-export default async <T>(slug: string) => {
-  const { data } = useAsyncData(`${slug}-page-data`, async () => {
-    const contentContainers = await useFetchWithCache(`/api/settings`);
-    return contentContainers;
-  });
-
-  return data;
+export default async <T>() => {
+  const response = await useFetchWithCache(`/api/settings`);
+  return response;
 };
--- composables/usePage.ts
+++ composables/usePage.ts
@@ -1,8 +1,4 @@
 export default async <T>(slug: string) => {
-  const { data } = useAsyncData(`${slug}-page-data`, async () => {
-    const contentContainers = await useFetchWithCache(`/api/discover/${slug}`);
-    return contentContainers;
-  });
-
-  return data;
+  const response = await useFetchWithCache(`/api/discover/${slug}`);
+  return response;
 };
